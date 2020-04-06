cf4c4831d69ab6a89aa4bbaeb56f33043c9120d8
Marouane HALAOIT <marouane_halaoit@carrefour.com>
2020-04-06 15:07:39 +0200

    Update the plugin

--- tags/plugin.py
+++ tags/plugin.py
@@ -34,6 +34,7 @@ class TagsPlugin(BasePlugin):
         self.tags_filename = "tags.md"
         self.tags_folder = "aux"
         self.tags_template = None
+        self.tag_pages_filename_template = "tag.{tag}.md"
 
     def on_nav(self, nav, config, files):
         # nav.items.insert(1, nav.items.pop(-1))
@@ -59,19 +60,43 @@ class TagsPlugin(BasePlugin):
                 continue
             self.metadata.append(get_metadata(f.src_path, config["docs_dir"]))
 
-        # Create new file with tags
-        self.generate_tags_file()
+        # Create new files with tags
+        generated_files = self.generate_files()
 
         # New file to add to the build
-        newfile = File(
-            path=str(self.tags_filename),
-            src_dir=str(self.tags_folder),
-            dest_dir=config["site_dir"],
-            use_directory_urls=False
-        )
-        files.append(newfile)
+        for generated_file in generated_files:
+            newfile = File(
+                path=str(generated_file),
+                src_dir=str(self.tags_folder),
+                dest_dir=config["site_dir"],
+                use_directory_urls=False
+            )
+            files.append(newfile)
 
-    def generate_tags_page(self, data):
+    def generate_files(self):
+        all_generated_files = []
+
+        # Get aggregated data
+        data = self.__get_aggregated_data()
+
+        # Dict that maps tag with the file containing its pages
+        d = dict()
+
+        # 1 - Create pages per tag files
+        for tag, pages in data:
+            filename = self.tag_pages_filename_template.format(tag=tag)
+            templ_path = Path(__file__).parent  / Path("templates")
+            environment = jinja2.Environment(
+                loader=jinja2.FileSystemLoader(str(templ_path))
+                )
+            templ = environment.get_template("tag-pages.md.template")
+            output_text = templ.render(tag=tag, pages=pages)
+            file_path = str(self.tags_folder / filename)
+            self.__write_file(output_text, file_path)
+            d[tag] = filename
+            all_generated_files.append(filename)
+        
+        # 2 - Create the global tags file
         if self.tags_template is None:
             templ_path = Path(__file__).parent  / Path("templates")
             environment = jinja2.Environment(
@@ -83,12 +108,14 @@ class TagsPlugin(BasePlugin):
                 loader=jinja2.FileSystemLoader(searchpath=str(self.tags_template.parent))
             )
             templ = environment.get_template(str(self.tags_template.name))
-        output_text = templ.render(
-                tags=sorted(data.items(), key=lambda t: t[0].lower()),
-        )
-        return output_text
+        output_text = templ.render(data=d.items())
+        file_path = str(self.tags_folder / self.tags_filename)
+        self.__write_file(output_text, file_path)
+        all_generated_files.append(self.tags_filename)
 
-    def generate_tags_file(self):
+        return all_generated_files
+
+    def __get_aggregated_data(self):
         sorted_meta = sorted(self.metadata, key=lambda e: e.get("year", 5000) if e else 0)
         tag_dict = defaultdict(list)
         for e in sorted_meta:
@@ -100,11 +127,11 @@ class TagsPlugin(BasePlugin):
             if tags is not None:
                 for tag in tags:
                     tag_dict[tag].append(e)
+        return sorted(tag_dict.items(), key=lambda t: t[0].lower())
 
-        t = self.generate_tags_page(tag_dict)
-
-        with open(str(self.tags_folder / self.tags_filename), "w") as f:
-            f.write(t)
+    def __write_file(self, data, file_path):
+        with open(file_path, "w") as f:
+            f.write(data)
 
 # Helper functions
 
